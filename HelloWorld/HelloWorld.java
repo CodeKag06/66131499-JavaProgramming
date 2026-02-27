995df5f2b9b3ad7c8bdddc549edaa6876ae9b5b6
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-02-27 15:29:59 +0700

    update
    thay đổi lời chào và thêm thông tin cá nhân

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("xin chao the gioi!");
+        System.out.println("Nguyen Phuc Khang - 2006");
     }
 }
