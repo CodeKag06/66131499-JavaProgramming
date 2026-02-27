9843934a2f807c24264e441e474d5b7d3abea54b
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-02-27 17:17:40 +0700

    thực hiện các phép tính +-*/
    thực hiện  +-*/ giữa 5 với 5

---
+++ TinhToanDG/TinhToanDG.java
@@ -0,0 +1,8 @@
+public class TinhToanDG {
+    public static void main(String[] args) {
+        System.out.println("5 + 5 = " + (5 + 5));
+        System.out.println("5 - 5 = " + (5 - 5));
+        System.out.println("5 * 5 = " + (5 * 5));
+        System.out.println("5 / 5 = " + (5 / 5));
+    }
+}
