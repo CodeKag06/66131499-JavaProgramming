e5b7293317aad32411026097aef5ac0232f77837
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-02-28 16:16:12 +0700

    code và chạy các ví dụ HelloWorld

---
+++ HelloWorldHD/Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    public static void main (String[] args) {
+        // text block voi """
+        String HelloJv = """
+            ============================
+             Hello Java!
+            ============================
+            by Nguyen Phuc Khang svNTU
+            ============================
+        """;
+        System.out.println(HelloJv);
+    }
+}
---
+++ HelloWorldHD/HelloW.java
@@ -0,0 +1,12 @@
+
+public class HelloW {
+    public static void main (String[] args) {
+        // su dung var de khai bao bien
+       var message = "Hello World with var!";
+       var number = 42;
+       var list = java.util.Arrays.asList("Java", "Modern", "Features");
+       System.out.println(message);
+       System.out.println("Number: " + number);
+       System.out.println("List: " + list);
+    }
+}
