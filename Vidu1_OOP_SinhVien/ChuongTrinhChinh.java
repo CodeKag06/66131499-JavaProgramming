6cdba20f9c4950cdcd47aabae4dafbd076895453
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-03-12 05:30:18 +0700

    thử nghiệm
    tạo mới đối tượng sinh viên

---
+++ Vidu1_OOP_SinhVien/ChuongTrinhChinh.java
@@ -0,0 +1,11 @@
+public class ChuongTrinhChinh {
+    public static void main(String[] args) {
+        //tao doi tuong sinh vien
+        SinhVien sv1 = new SinhVien();
+        //cap nhat gia tri cu the
+        sv1.ten = "Nguyen Phuc Khang";
+        sv1.msv = "SV001";
+        sv1.tuoi = 19;
+        sv1.hocBai("Lap trinh Java");
+    }
+}
---
+++ Vidu1_OOP_SinhVien/SinhVien.java
@@ -0,0 +1,11 @@
+public class SinhVien {
+    //dinh nghia cac thuoc tinh
+    String ten;
+    String msv;
+    int tuoi;
+
+    //dinh nghia cac phuong thuc
+    void hocBai (String monhoc) {
+        System.out.println("Ban " + ten + " dang hoc bai mon " + monhoc);
+    }
+}
