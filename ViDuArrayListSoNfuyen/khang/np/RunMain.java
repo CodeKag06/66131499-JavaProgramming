d5b445835a2408c4c791b7f93d12acd738dec8f3
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-03-18 20:00:14 +0700

    Khai báo biến danh sách

---
+++ ViDuArrayListSoNfuyen/khang/np/RunMain.java
@@ -0,0 +1,11 @@
+package khang.np;
+
+import java.util.ArrayList;
+
+public class RunMain {
+	public static void main(String[] args) {
+		//Khai báo biến danh sách, chứa các số nguyên
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		
+	}
+}
