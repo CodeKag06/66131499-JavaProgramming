1b268e98fb08382eb29bdb917932602bc4c07c82
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-04-23 07:49:48 +0700

    khai bao và nhap cjieu cao can nang, tính BMI

---
+++ BaiThiGK/src/thigk2/NguyenPhucKhang/Cau1.java
@@ -0,0 +1,16 @@
+package thigk2.NguyenPhucKhang;
+
+import java.util.Scanner;
+
+public class Cau1 {
+
+	public static void main(String[] args) {
+		System.out.println("moi ban nhap chieu cao: ");
+		System.out.println("moi ban nhap can nang: ");
+		Scanner banPhim = new Scanner(System.in);
+		float chieuCao = banPhim.nextFloat();
+		float canNang = banPhim.nextFloat();
+		
+	}
+
+}
