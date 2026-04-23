b7083f13e3294b2a11467eccbda031c88bc5d911
hapikag <khang.np.66cntt@ntu.edu.vn>
2026-04-23 07:57:00 +0700

    hoàn thành Câu1

--- BaiThiGK/src/thigk2/NguyenPhucKhang/Cau1.java
+++ BaiThiGK/src/thigk2/NguyenPhucKhang/Cau1.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class Cau1 {
 
 	public static void main(String[] args) {
-		System.out.println("moi ban nhap chieu cao: ");
-		System.out.println("moi ban nhap can nang: ");
+		// nhap du lieu tu ban phim
 		Scanner banPhim = new Scanner(System.in);
+		System.out.print("moi ban nhap chieu cao(don vi m): ");
 		float chieuCao = banPhim.nextFloat();
+		System.out.print("\nmoi ban nhap can nang(don vi kg): ");
 		float canNang = banPhim.nextFloat();
-		
+		float BMI = chieuCao/canNang;
+		// dua ra ket qua suc khoe
+		if(BMI < 18.5) {
+			System.out.print("BMI =" + BMI +" ban qua gay");
+		}
+		else if(BMI > 24.9) {
+			System.out.print("BMI =" + BMI +" ban qua beo");
+		}
+		else {
+			System.out.print("BMI =" + BMI +" ban qua khoe manh");
+		}
 	}
 
 }
